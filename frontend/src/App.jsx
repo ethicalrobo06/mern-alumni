fa8c6fee87720bbf79106d952d78b0b3f7e7a818
Chandan <ethicalrobo06@gmail.com>
2024-07-21 00:29:04 +0530

    remove /signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Foet from './screen/Foet'
 import Fod from './screen/Fod'
 import Home from './components/Home'
 import Login from './pages/Login'
-import Signup from './pages/Signup'
 import { useState } from 'react'
 import RefreshHandler from './pages/RefreshHandler'
 
@@ -21,7 +20,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Navigate to='/login' />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
         <Route path='/home' element={<PrivateRoute element={<Home />} />} />
         <Route path='/foet' element={<PrivateRoute element={<Foet />} />} />
         <Route path='/fod' element={<PrivateRoute element={<Fod />} />} />
